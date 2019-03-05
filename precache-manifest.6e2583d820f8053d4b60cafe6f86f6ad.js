be8c8feeb219eb7fb656eee4d4581826cc159b52
Mossy <bridgo.mcmahon@gmail.com>
2019-03-05 20:46:08 +1100

    Updates

---
+++ precache-manifest.6e2583d820f8053d4b60cafe6f86f6ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b623c8ab996541b76634",
+    "url": "/project3/static/css/main.ef481f4e.chunk.css"
+  },
+  {
+    "revision": "b623c8ab996541b76634",
+    "url": "/project3/static/js/main.b623c8ab.chunk.js"
+  },
+  {
+    "revision": "45b898d2506cf3a35cb9",
+    "url": "/project3/static/js/runtime~main.45b898d2.js"
+  },
+  {
+    "revision": "e1ddca98264f40ca686f",
+    "url": "/project3/static/js/2.e1ddca98.chunk.js"
+  },
+  {
+    "revision": "ea1b3a5d0edcdf05736d369ef3ddc0a3",
+    "url": "/project3/index.html"
+  }
+];
